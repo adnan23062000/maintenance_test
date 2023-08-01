7becca5a54f074435f4752acb9668ec98d6386a3
adnan23062000 <bsse1131@iit.du.ac.bd>
2023-08-01 12:22:18 +0600

    2nd commit

--- input.c
+++ input.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int main()
+void main()
 {
-   // This variable is to store the input number 
+   
    int num;
  
    printf("Enter an integer: ");
@@ -10,8 +10,8 @@ int main()
    // Modulus (%) returns remainder
    if ( num%2 == 0 )
       printf("%d is an even number", num);
+   else if((num%2)!=0 && num>=0)
+      printf("%d is a positive odd number", num);
    else
-      printf("%d is an odd number", num);
- 
-   return 0;
+      printf("%d is a negative odd number", num);
 }
